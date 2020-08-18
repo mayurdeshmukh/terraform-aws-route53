894431fde024ce92a87b0a2ded3355a72f4a7b27
mayurdeshmukh <33214046+mayurdeshmukh@users.noreply.github.com>
2020-08-18 15:30:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,47 +9,47 @@ resource "aws_route53_zone" "this" {
 
 resource "aws_route53_record" "this" {
   depends_on  = [aws_route53_zone.this]
-  count = "${var.zone_id == "false" && length(var.records["names"]) > 0 ? length(var.records["names"]) : 0}"
-  zone_id = "${var.zone_id != "false" ? var.zone_id : aws_route53_zone.this.0.zone_id}"
+  count = var.zone_id == "false" && length(var.records["names"]) > 0 ? length(var.records["names"]) : 0
+  zone_id = var.zone_id != "false" ? var.zone_id : aws_route53_zone.this.0.zone_id
   name = "${element(var.records["names"], count.index)}${var.domain}"
-  type = "${element(var.records["types"], count.index)}"
-  ttl = "${element(var.records["ttls"], count.index)}"
-  records = "${split(",", element(var.records["values"], count.index))}"
+  type = element(var.records["types"], count.index)
+  ttl = element(var.records["ttls"], count.index)
+  records = split(",", element(var.records["values"], count.index))
 }
 
 resource "aws_route53_record" "alias" {
   depends_on  = [aws_route53_zone.this]
-  count = "${var.zone_id == "false" && length(var.alias["names"]) > 0 ? length(var.alias["names"]) : 0}"
-  zone_id = "${var.zone_id != "false" ? var.zone_id : aws_route53_zone.this.0.zone_id}"
+  count = var.zone_id == "false" && length(var.alias["names"]) > 0 ? length(var.alias["names"]) : 0
+  zone_id = var.zone_id != "false" ? var.zone_id : aws_route53_zone.this.0.zone_id
   name = "${element(var.alias["names"], count.index)}${var.domain}"
   type = "A"
 
   alias {
-    name  = "${element(var.alias["values"], count.index)}"
-    zone_id = "${element(var.alias["zones_id"], count.index)}"
+    name  = element(var.alias["values"], count.index)
+    zone_id = element(var.alias["zones_id"], count.index)
     evaluate_target_health = false
   }
 
 }
 
 resource "aws_route53_record" "imported" {
-  count = "${var.zone_id != "false" && length(var.records["names"]) > 0 ? length(var.records["names"]) : 0}"
+  count = var.zone_id != "false" && length(var.records["names"]) > 0 ? length(var.records["names"]) : 0
   zone_id = var.zone_id
   name = "${element(var.records["names"], count.index)}${var.domain}"
-  type = "${element(var.records["types"], count.index)}"
-  ttl = "${element(var.records["ttls"], count.index)}"
-  records = "${split(",", element(var.records["values"], count.index))}"
+  type = element(var.records["types"], count.index)
+  ttl = element(var.records["ttls"], count.index)
+  records = split(",", element(var.records["values"], count.index))
 }
 
 resource "aws_route53_record" "alias_imported" {
-  count = "${var.zone_id != "false" && length(var.alias["names"]) > 0 ? length(var.alias["names"]) : 0}"
+  count = var.zone_id != "false" && length(var.alias["names"]) > 0 ? length(var.alias["names"]) : 0
   zone_id = var.zone_id
   name = "${element(var.alias["names"], count.index)}${var.domain}"
   type = "A"
 
   alias {
-    name  = "${element(var.alias["values"], count.index)}"
-    zone_id = "${element(var.alias["zones_id"], count.index)}"
+    name  = element(var.alias["values"], count.index)
+    zone_id = element(var.alias["zones_id"], count.index)
     evaluate_target_health = false
   }
 
